9df788c429c713f661efcd122a3e1e5b6daad067
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2023-10-30 23:45:20 +0800

    added lombok

--- src/main/java/com/dylan/notes/model/Note.java
+++ src/main/java/com/dylan/notes/model/Note.java
@@ -2,57 +2,19 @@ package com.dylan.notes.model;
 
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
 public class Note {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String content;
-//
-//    @ManyToOne
-//    private User user;
-
-
-    public Note() {
-    }
-
-    public Note(String title, String content, User user) {
-        this.title = title;
-        this.content = content;
-//        this.user = user;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-//    public User getUser() {
-//        return user;
-//    }
-//
-//    public void setUser(User user) {
-//        this.user = user;
-//    }
 }
--- src/main/java/com/dylan/notes/model/User.java
+++ src/main/java/com/dylan/notes/model/User.java
@@ -1,11 +1,17 @@
 package com.dylan.notes.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -13,59 +19,7 @@ public class User {
     private String username;
     private String password;
 
-    // establish a one-to-many relationship between User and Note
-//    @OneToMany(mappedBy = "user")
-//    private List<Note> notes;
-
-
-    public User() {
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-//        this.notes = notes;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-//    public List<Note> getNotes() {
-//        return notes;
-//    }
-//
-//    public void setNotes(List<Note> notes) {
-//        this.notes = notes;
-//    }
-
-
-//    @Override
-//    public String toString() {
-//        return "User{" +
-//                "id=" + id +
-//                ", username='" + username + '\'' +
-//                ", password='" + password + '\'' +
-//                '}';
-//    }
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "owner", referencedColumnName = "id")
+    private List<Note> notes;
 }
