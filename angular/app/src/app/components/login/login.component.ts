d2588b6cf25352ef56937690346022ea8e9b9871
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2023-10-31 21:20:29 +0800

    removed unused fontawesome icon

--- angular/app/src/app/components/login/login.component.ts
+++ angular/app/src/app/components/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {faGoogle} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +6,4 @@ import {faGoogle} from '@fortawesome/free-brands-svg-icons';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  faGoogle = faGoogle;
 }
